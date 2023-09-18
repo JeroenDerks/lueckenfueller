074ff1a87f58d461d0810b16f1b653a0fc9c149b
jeroen.derks@labela.nl <jeroen.derks@labela.nl>
2023-09-18 23:05:55 +0200

    feat: test without prisma

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,29 +21,29 @@ export default function Home({ needs }: { needs: Need[] }) {
 }
 
 export const getStaticProps = async ({ locale }: { locale: any }) => {
-  const needs = await prisma.need.findMany({
-    include: {
-      likes: true,
-      location: true,
-    },
-  });
+  // const needs = await prisma.need.findMany({
+  //   include: {
+  //     likes: true,
+  //     location: true,
+  //   },
+  // });
 
   return {
     props: {
       ...(await serverSideTranslations(locale, ["common"])),
-      needs: needs.map((need) => {
-        return {
-          ...need,
-          createdAt: need.createdAt.toISOString(),
-          updatedAt: need.updatedAt.toISOString(),
-          likes: need.likes.map((like) => {
-            return {
-              ...like,
-              createdAt: like.createdAt.toISOString(),
-            };
-          }),
-        };
-      }),
+      // needs: needs.map((need) => {
+      //   return {
+      //     ...need,
+      //     createdAt: need.createdAt.toISOString(),
+      //     updatedAt: need.updatedAt.toISOString(),
+      //     likes: need.likes.map((like) => {
+      //       return {
+      //         ...like,
+      //         createdAt: like.createdAt.toISOString(),
+      //       };
+      //     }),
+      //   };
+      // }),
     },
   };
 };
