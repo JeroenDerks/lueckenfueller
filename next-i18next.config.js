5627327f19f17b6559f526b14098e78a07b7615d
jeroen.derks@labela.nl <jeroen.derks@labela.nl>
2023-10-15 18:47:45 +0200

    feat: new test

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,18 @@
-module.exports = {
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en", "de"],
-  },
+const path = require("path");
+
+/** @type import("next").I18NConfig */
+const i18n = {
+  defaultLocale: "en",
+  locales: ["en", "de"],
+  localeDetection: false, // disable automatic redirection on the user's preferred locale
 };
+
+/** @type import("next-i18next").UserConfig */
+const next18nextConfig = {
+  i18n,
+  fallbackLng: "en",
+  localePath: path.resolve("./public/locales"),
+  reloadOnPrerender: process.env.NODE_ENV === "development",
+};
+
+module.exports = next18nextConfig;
