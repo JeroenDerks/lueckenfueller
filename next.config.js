b10e86349673c091418dce13fb648e24922608aa
jeroen.derks@labela.nl <jeroen.derks@labela.nl>
2023-10-15 18:38:47 +0200

    feat: prevent locale detection

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require("./next-i18next.config");
-const path = require("path");
 
 const nextConfig = {
   reactStrictMode: true,
   i18n,
-  localePath: path.resolve("./public/locales"),
 };
 
 module.exports = nextConfig;
