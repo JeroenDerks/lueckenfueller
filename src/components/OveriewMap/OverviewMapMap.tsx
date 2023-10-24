b083832765fee6d6df0498ec7dc837a1fe49d76f
jeroen.derks@labela.nl <jeroen.derks@labela.nl>
2023-10-24 20:29:33 +0200

    feat: add popup to overview map

---
+++ src/components/OveriewMap/OverviewMapMap.tsx
@@ -0,0 +1,61 @@
+import React, { useMemo, useState } from "react";
+import { Popup } from "react-map-gl";
+
+import { Need } from "@/types";
+
+import { MapComp } from "../MapComp";
+import { RegularMarker } from "../MapMarkers/RegularMarker";
+import { useRouter } from "next/router";
+import { Box, Typography } from "@mui/material";
+import Link from "next/link";
+
+export const OverviewMapMap = ({ needs }: { needs?: Need[] }) => {
+  const [selectedNeed, setSelectedNeed] = useState<Need | false>(false);
+  const router = useRouter();
+
+  const markers = useMemo(
+    () =>
+      needs?.map((need) => (
+        <RegularMarker
+          key={need.id}
+          id={need.id}
+          latitude={need.location?.lat!}
+          longitude={need.location?.lng!}
+          // @ts-ignore typing of event.originalEvent to prevent propogation which in turn is needed not close modal immediately
+          onClick={(e) => {
+            e?.originalEvent.stopPropagation();
+            setSelectedNeed(need);
+          }}
+          radius={need.location?.radius!}
+        />
+      )),
+    [needs]
+  );
+
+  return (
+    <MapComp>
+      {markers}
+      {selectedNeed && needs?.includes(selectedNeed) && (
+        <Popup
+          longitude={selectedNeed.location?.lng!}
+          latitude={selectedNeed.location?.lat!}
+          anchor="bottom"
+          style={{ zIndex: 10 }}
+          onClose={() => setSelectedNeed(false)}
+        >
+          <Box minWidth={220}>
+            <Typography variant="body1" fontWeight="700">
+              {selectedNeed.title}
+            </Typography>
+            <Box display="flex" width={1} justifyContent="space-between">
+              <Typography variant="body2">
+                {selectedNeed.likes?.length} Likes
+              </Typography>
+              <Link href={`/${selectedNeed.id}`}>More info</Link>
+            </Box>
+          </Box>
+        </Popup>
+      )}
+    </MapComp>
+  );
+};
--- src/components/OveriewMap/index.tsx
+++ src/components/OveriewMap/index.tsx
@@ -1,25 +1,21 @@
 import React, { useEffect, useState } from "react";
-import Box from "@mui/material/Box";
 import Link from "next/link";
+import Box from "@mui/material/Box";
+import { Tooltip } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 
 import { PageLayout } from "@/components/PageLayout";
 import { theme } from "@/styles/theme";
 import { Need } from "@/types";
 
-import { MapComp } from "../MapComp";
-import { RegularMarker } from "../MapMarkers/RegularMarker";
-import { useRouter } from "next/router";
 import { NeedSelector } from "../NeedSelector";
-import { MapSelectorContainer } from "../MapController/styled";
 import Button from "@mui/material/Button";
 import { MapControlContainer } from "./styled";
-import { IconButton, Tooltip } from "@mui/material";
+import { OverviewMapMap } from "./OverviewMapMap";
 
 export const OverviewMap = () => {
   const [needs, setNeeds] = useState<Need[]>();
   const [category, setCategory] = useState<string>();
-  const router = useRouter();
 
   useEffect(() => {
     const listNeeds = async () => {
@@ -31,7 +27,7 @@ export const OverviewMap = () => {
       if (response.ok) {
         const data = await response.json();
         if (data) {
-          setNeeds(data);
+          setNeeds(data as Need[]);
         }
       }
     };
@@ -54,18 +50,7 @@ export const OverviewMap = () => {
           </Tooltip>
         </MapControlContainer>
       </Box>
-      <MapComp>
-        {needs?.map(({ location, id }) => (
-          <RegularMarker
-            key={id}
-            id={id}
-            latitude={location?.lat!}
-            longitude={location?.lng!}
-            onClick={() => router.push({ pathname: `/${id}` })}
-            radius={location?.radius!}
-          />
-        ))}
-      </MapComp>
+      <OverviewMapMap needs={needs} />
     </PageLayout>
   );
 };
