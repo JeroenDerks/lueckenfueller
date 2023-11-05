22a5690bc699ad0b65aa3a2ad37c244aff184176
jeroen.derks@labela.nl <jeroen.derks@labela.nl>
2023-11-05 12:48:22 +0100

    feat: test border radius overflow fix

--- src/components/MapComp/styled.ts
+++ src/components/MapComp/styled.ts
@@ -10,4 +10,6 @@ export const Container = styled("div")({
   overflow: "hidden",
   position: "relative",
   width: "100%",
+  maskImage:
+    "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAA5JREFUeNpiYGBgAAgwAAAEAAGbA+oJAAAAAElFTkSuQmCC)" /* this fixes the overflow:hidden in Chrome/Opera */,
 });
