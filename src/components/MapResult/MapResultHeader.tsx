98ac03cfbe8203fb7e775450b9c965695f6e43b0
jeroen.derks@labela.nl <jeroen.derks@labela.nl>
2023-11-19 17:14:29 +0100

    feat: update translations

--- src/components/MapResult/MapResultHeader.tsx
+++ src/components/MapResult/MapResultHeader.tsx
@@ -2,17 +2,18 @@ import React, { useEffect, useState } from "react";
 import { Need } from "@/types";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import { Grid } from "@mui/material";
+import { Badge, Grid } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import { Container } from "./styled";
 import { useTranslation } from "next-i18next";
 import MapResultLikeModal from "./MapResultLikeModal";
+import useTranslatedOptions from "@/hooks/getTranslatedOptions";
 
 export default function MapResultHeader({ need }: { need?: Need }) {
   const [currNeed, setCurrNeed] = useState<Need | undefined>(need);
   const [openModal, setOpenModal] = React.useState(false);
-
+  const { translatedOptions } = useTranslatedOptions();
   const { t } = useTranslation();
 
   useEffect(() => {
@@ -22,6 +23,9 @@ export default function MapResultHeader({ need }: { need?: Need }) {
   }, [need, currNeed?.id]);
 
   if (typeof window === "undefined" || !need) return null;
+  const translatedNeed = translatedOptions?.find(
+    ({ value }) => value === currNeed?.title
+  );
 
   return (
     <>
@@ -29,21 +33,33 @@ export default function MapResultHeader({ need }: { need?: Need }) {
         <Grid container justifyContent="space-between" alignItems="center">
           <Grid item xs={12} sm={12} md={10}>
             <Typography variant="h5">
-              {t("MapResult.titlePrefix")}: {currNeed?.title}
+              {t("MapResult.titlePrefix")}: {translatedNeed?.label}
+            </Typography>
+            <Typography variant="body2" mr={1}>
+              {t("MapResult.likeCountSuffix", {
+                count: currNeed?.likes?.length || 0,
+              })}
             </Typography>
           </Grid>
           <Grid item xs={12} sm={12} md={2}>
             <Box
+              alignItems={["center", "center", "flex-end"]}
               display="flex"
-              alignItems="center"
-              justifyContent={["flex-start", "flex-start", "flex-end"]}
+              flexDirection={["row", "row", "column"]}
+              mt={[1, 1, 0]}
             >
-              <Typography variant="body2" mr={1}>
-                {currNeed?.likes?.length || 0} Likes
-              </Typography>
-              <IconButton color="primary" onClick={() => setOpenModal(true)}>
-                <ThumbUpIcon />
+              <IconButton
+                color="primary"
+                onClick={() => setOpenModal(true)}
+                size="large"
+              >
+                <Badge badgeContent={currNeed?.likes?.length} color="secondary">
+                  <ThumbUpIcon />
+                </Badge>
               </IconButton>
+              <Typography variant="body2" ml={[1, 1, 0]}>
+                {t("MapResult.likePrefix")}
+              </Typography>
             </Box>
           </Grid>
         </Grid>
