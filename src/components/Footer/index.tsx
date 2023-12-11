e4438fa12d60b333a217c44e9566ebca0e76f2f7
jeroen.derks@labela.nl <jeroen.derks@labela.nl>
2023-12-11 19:15:59 +0100

    feat: fix logo in footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,22 +1,14 @@
 import { Grid } from "@mui/material";
 import { PageLayout } from "../PageLayout";
 import { Link } from "../Link";
-import Image from "next/image";
+import { Logo } from "../Logo";
 
 export const Footer = () => {
-  const imageSize = { w: 623, h: 134 };
-  const scale = 0.25;
-
   return (
     <PageLayout>
       <Grid container>
         <Grid item xs={12} sm={6} md={6}>
-          <Image
-            src="/lueckenfueller_logo.jpg"
-            width={imageSize.w * scale}
-            height={imageSize.h * scale}
-            alt="logo"
-          />
+          <Logo scale={0.2} />
         </Grid>
         <Grid item xs={6} sm={3} md={3}>
           <Link href="/privacy">Datenschutz&shy;erkl&auml;rung</Link>
