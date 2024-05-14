fc7f102c7f2029bfd4a2bb2dbcb37ff2a77338f8
jeroen.derks@labela.nl <jeroen.derks@labela.nl>
2024-05-14 21:25:07 +0200

    feat: add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,33 @@ import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import { ThemeProvider } from "@emotion/react";
 import { theme } from "@/styles/theme";
+import Script from "next/script";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <main>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </main>
+    <>
+      {process.env.NODE_ENV && process.env.NODE_ENV !== "development" && (
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-FN6M1RCX9Y`}
+          strategy="afterInteractive"
+        />
+      )}
+      {process.env.NODE_ENV && process.env.NODE_ENV !== "development" && (
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+                function gtag(){window.dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-FN6M1RCX9Y');
+            `}
+        </Script>
+      )}
+
+      <main>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </main>
+    </>
   );
 };
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -9,6 +10,23 @@ export default function Document() {
           rel="stylesheet"
         />
       </Head>
+
+      {process.env.NODE_ENV && process.env.NODE_ENV !== "development" && (
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-FN6M1RCX9Y`}
+          strategy="afterInteractive"
+        />
+      )}
+      {process.env.NODE_ENV && process.env.NODE_ENV !== "development" && (
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+                function gtag(){window.dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-FN6M1RCX9Y');
+            `}
+        </Script>
+      )}
+
       <body style={{ margin: 0, boxSizing: "border-box" }}>
         <Main />
         <NextScript />
